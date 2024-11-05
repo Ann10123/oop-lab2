69118a538e024618b897919529bc36d9d0cd0706
Ann10123 <152207473+Ann10123@users.noreply.github.com>
2024-11-05 22:30:22 +0200

    Add files via upload

---
+++ MyFrac.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+class MyFrac
+{
+    public long Nom { get; set; } 
+    public long Denom { get; set; } 
+    public MyFrac(long nom, long denom)
+    {
+        if (denom == 0)
+        {
+            throw new ArgumentException("Знаменник не може бути 0!");
+        }
+        Nom = nom;
+        Denom = denom;
+        Normalize();
+    }
+    private void Normalize()
+    {
+        if (Denom < 0)
+        {
+            Nom = -Nom;
+            Denom = -Denom;
+        }
+        long gcd = GCD(Math.Abs(Nom), Math.Abs(Denom));
+        if (gcd > 1)
+        {
+            Nom /= gcd;
+            Denom /= gcd;
+        }
+    }
+
+    private static long GCD(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public override string ToString()
+    {
+        return ($"{Nom}/{ Denom}");
+    }
+    public string ToStringWithIntPart()
+    {
+        if (Math.Abs(Nom) < Denom)
+        {
+            return ToString();
+        }
+        long wholePart = Nom / Denom;
+        long remainder = Math.Abs(Nom % Denom);
+
+        return remainder == 0
+            ? $"{wholePart}"
+            : $"{wholePart}+({remainder}/{Denom})";
+    }
+
+    public double ToDouble() => (double)Nom / Denom;
+
+    public static MyFrac operator +(MyFrac f1, MyFrac f2)
+    {
+        return new MyFrac(f1.Nom * f2.Denom + f2.Nom * f1.Denom, f1.Denom * f2.Denom);
+    }
+    public static MyFrac operator -(MyFrac f1, MyFrac f2)
+    {
+        return new MyFrac(f1.Nom * f2.Denom - f2.Nom * f1.Denom, f1.Denom * f2.Denom);
+    }
+    public static MyFrac operator *(MyFrac f1, MyFrac f2)
+    {
+        return new MyFrac(f1.Nom * f2.Nom, f1.Denom * f2.Denom);
+    }
+    public static MyFrac operator /(MyFrac f1, MyFrac f2)
+    {
+        if (f2.Nom == 0)
+        {
+            throw new DivideByZeroException("Ділити на 0 не можна!");
+        }
+        return new MyFrac(f1.Nom * f2.Denom, f1.Denom * f2.Nom);
+    }
+    public static MyFrac CalcExpr1(int n)
+    {
+        MyFrac result = new MyFrac(0, 1);
+        for (int i = 1; i * (i + 1) <= n * (n + 1); i++)
+        {
+            MyFrac add = new MyFrac(1, i * (i + 1));
+            result += add;
+        }
+        return result;
+    }
+
+    public static MyFrac CalcExpr2(int n)
+    {
+        MyFrac result = new MyFrac(1, 1);
+        for (long i = 2; i <= n; i++)
+        {
+            MyFrac term = new MyFrac(i * i - 1, i * i);
+            result *= term;
+        }
+        return result;
+    }
+}
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,131 +1,27 @@
-﻿namespace ConsoleApp85
+﻿class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            try
-            {
-                Console.WriteLine("Елементи 1-ої матриці:");
-                MyMatrix matrix_1 = Input();
-                Console.WriteLine("Елементи 2-ої матриці:");
-                MyMatrix matrix_2 = Input();
-                Operations(matrix_1, matrix_2);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-        static void Operations(MyMatrix matrix_1, MyMatrix matrix_2)
-        {
-            MyMatrix matrix_11 = matrix_1;
-            MyMatrix matrix_12 = matrix_2;
-            Console.WriteLine("1-ша транспонована матриця:");
-            matrix_1.TransponeMe();
-            matrix_1.PrintMatrix();
-            Console.WriteLine("2-га транспонована матриця:");
-            matrix_2.TransponeMe();
-            matrix_2.PrintMatrix();
-            Console.WriteLine("Множення матриць: ");
-            (matrix_11 * matrix_12).PrintMatrix();
-            Console.WriteLine("Сума матриць: ");
-            (matrix_11 + matrix_12).PrintMatrix();
-            Console.Write("Визначник 1-ої матриці: ");
-            double determinant1 = matrix_1.CalcDeterminant();
-            Console.Write(determinant1);
-            Console.WriteLine();
-            Console.Write("Визначник 2-ої матриці: ");
-            double determinant2 = matrix_2.CalcDeterminant();
-            Console.Write(determinant2);
-            Console.WriteLine();
-        }
-        static MyMatrix Input()
-        {
-            Console.WriteLine("2 - ввести самостійно двовимірний масив");
-            Console.WriteLine("3 - ввести зубчастий масив");
-            Console.WriteLine("4 - ввести масив рядків");
-            Console.WriteLine("5 - ввести матрицю як один рядок");
-            MyMatrix originalMatrix;
-            int choice = int.Parse(Console.ReadLine() ?? "0");
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            switch (choice)
-            {
-                case 2:
-                    Console.Write("К-сть рядків та стовпчиків: ");
-                    string[] data2 = Console.ReadLine().Trim().Split();
-                    int rows2 = int.Parse(data2[0]);
-                    int cols2 = int.Parse(data2[1]);
+        Console.Write("Введіть 1-ий дріб (чисельник та знаменник розділені пробілом): ");
+        string[] input1 = Console.ReadLine().Trim().Split();
+        MyFrac f1 = new MyFrac(long.Parse(input1[0]), long.Parse(input1[1]));
 
-                    double[,] elements2 = new double[rows2, cols2];
-                    Console.WriteLine("Введіть елементи матриці: ");
-                    for (int i = 0; i < rows2; i++)
-                    {
-                        for (int j = 0; j < cols2; j++)
-                        {
-                            elements2[i, j] = double.Parse(Console.ReadLine());
-                        }
-                    }
-                    originalMatrix = new MyMatrix(elements2);
-                    return originalMatrix;
+        Console.Write("Введіть 2-ий дріб (чисельник та знаменник розділені пробілом): ");
+        string[] input2 = Console.ReadLine().Trim().Split();
+        MyFrac f2 = new MyFrac(long.Parse(input2[0]), long.Parse(input2[1]));
 
-                case 3:
-                    Console.Write("К-сть рядків: ");
-                    int rows3 = int.Parse(Console.ReadLine());
+        Console.Write("Введіть значення n для розрахунків: ");
+        int n = int.Parse(Console.ReadLine());
 
-                    double[][] jaggedArray = new double[rows3][];
-                    for (int i = 0; i < rows3; i++)
-                    {
-                        Console.Write($"К-сть стовчиків у рядку {i + 1}: ");
-                        int cols3 = int.Parse(Console.ReadLine());
-                        jaggedArray[i] = new double[cols3];
-
-                        Console.WriteLine($"Введіть елементи рядку {i + 1}: ");
-                        for (int j = 0; j < cols3; j++)
-                        {
-                            jaggedArray[i][j] = double.Parse(Console.ReadLine());
-                        }
-                    }
-                    originalMatrix = new MyMatrix(jaggedArray);
-                    return originalMatrix;
-
-                case 4:
-                    Console.Write("К-сть рядків: ");
-                    int rows4 = int.Parse(Console.ReadLine());
-
-                    string[] stringArray = new string[rows4];
-                    Console.WriteLine("Введіть рядки значень: ");
-                    for (int i = 0; i < rows4; i++)
-                    {
-                        stringArray[i] = Console.ReadLine() ?? string.Empty;
-                    }
-                    originalMatrix = new MyMatrix(stringArray);
-                    return originalMatrix;
-
-                case 5:
-                    Console.WriteLine("Введіть елементи матриці");
-                    string input = Console.ReadLine();
-                    string[] elementsString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-                    int rows5 = int.Parse(elementsString[0]);
-                    int cols5 = int.Parse(elementsString[1]);
-                    double[,] elements5 = new double[rows5, cols5];
-
-                    for (int i = 0; i < rows5; i++)
-                    {
-                        for (int j = 0; j < cols5; j++)
-                        {
-                            elements5[i, j] = double.Parse(elementsString[(i * cols5) + j + 2]); // +2, щоб пропустити розміри
-                        }
-                    }
-                    originalMatrix = new MyMatrix(elements5);
-                    return originalMatrix;
-                default:
-                    double[,] elements6 = { { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 0 } };
-                    originalMatrix = new MyMatrix(elements6);
-                    return originalMatrix;
-            }
-        }
+        Console.WriteLine($"\nПерший дріб: {f1}");
+        Console.WriteLine($"Другий дріб: {f2}");
+        Console.WriteLine($"Скорочений перший дріб: {f1.ToStringWithIntPart()}");
+        Console.WriteLine($"Скорочений другий дріб: {f2.ToStringWithIntPart()}");
+        Console.WriteLine($"Перший дріб як десятковий: {f1.ToDouble()}");
+        Console.WriteLine($"Другий дріб як десятковий: {f2.ToDouble()}");
+        Console.WriteLine($"Результат обчислення для першого виразу: {MyFrac.CalcExpr1(n)}");
+        Console.WriteLine($"Результат обчислення для другого виразу: {MyFrac.CalcExpr2(n)}");
     }
 }
